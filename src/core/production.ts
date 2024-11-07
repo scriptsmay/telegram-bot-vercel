e2a853f2c9d539f477fbce628ad39967c4ca7060
scriptsmay <scriptsmay@163.com>
2024-11-07 14:20:50 +0800

    add send api

--- src/core/production.ts
+++ src/core/production.ts
@@ -2,11 +2,16 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import createDebug from 'debug';
 import { Context, Telegraf } from 'telegraf';
 import { Update } from 'telegraf/typings/core/types/typegram';
+import axios from 'axios';
 
 const debug = createDebug('bot:dev');
 
 const PORT = (process.env.PORT && parseInt(process.env.PORT, 10)) || 3000;
 const VERCEL_URL = `${process.env.VERCEL_URL}`;
+const TCKEY = process.env.TCKEY || '';
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const md5 = require('md5');
+
 
 const production = async (
   req: VercelRequest,
@@ -18,6 +23,39 @@ const production = async (
 
   if (!VERCEL_URL) {
     throw new Error('VERCEL_URL is not set.');
+  }
+
+  if (req.url?.substring(0, 9) == '/api/send') {
+    const text = req.query?.text || req.body?.text || '';
+    const sendkey = req.query?.sendkey || req.body?.sendkey || '';
+    const desp = req.query?.desp || req.body?.desp || '';
+    const markdown = req.query?.markdown || req.body?.markdown || '';
+
+    if (text == '' || sendkey == '') {
+      throw new Error('text & sendkey cannot be empty');
+    } else {
+      const key_info: String[] = String(sendkey).split('T');
+
+      if (key_info[1] != md5(TCKEY + key_info[0])) {
+        throw new Error('sendkey error');
+      } else {
+        var params = new URLSearchParams();
+        params.append('chat_id', String(key_info[0]));
+        let content = String(text) + '\n\n' + String(desp);
+        if (markdown != '') {
+          content += String(markdown);
+          params.append('parse_mode', 'MarkdownV2');
+        }
+
+        params.append('text', content);
+
+        const ret = await axios.post(
+          'https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage',
+          params,
+        );
+        res.status(200).json(ret.data);
+      }
+    }
   }
 
   const getWebhookInfo = await bot.telegram.getWebhookInfo();
